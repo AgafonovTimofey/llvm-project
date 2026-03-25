0699c8ee55a988786bddffcb9a9f88116951094a
TimofeyAgafonov <yantaya.p_o@mail.ru>
2026-03-25 15:43:43 +0300

    [TAga] 17. Add full TAgaInstPrinter

--- llvm/lib/Target/TAga/MCTargetDesc/TAgaInstPrinter.h
+++ llvm/lib/Target/TAga/MCTargetDesc/TAgaInstPrinter.h
@@ -13,14 +13,16 @@ public:
       : MCInstPrinter(MAI, MII, MRI) {}
 
   std::pair<const char *, uint64_t>
-  getMnemonic(const MCInst &MI) const override {
-    llvm_unreachable("TAgaInstPrinter getMnemonic not implemented");
-    return {"", 0};
-  }
+  getMnemonic(const MCInst &MI) const override;
+  void printInstruction(const MCInst *MI, uint64_t Address, raw_ostream &O);
 
-  void printRegName(raw_ostream &O, MCRegister Reg) override {}
+  static const char *getRegisterName(MCRegister Reg);
+  static void printOperand(const MCOperand &MO, raw_ostream &O);
+
+  void printRegName(raw_ostream &O, MCRegister Reg) override;
   void printInst(const MCInst *MI, uint64_t Address, StringRef Annot,
-                 const MCSubtargetInfo &STI, raw_ostream &O) override {}
+                 const MCSubtargetInfo &STI, raw_ostream &O) override;
+  void printOperand(const MCInst *MI, int OpNo, raw_ostream &OS);
 };
 } // namespace llvm
 
